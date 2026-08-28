df23510ba1c8a5897e598f912610f187b58c872d
Martin Messer <martin.messer@cyberus-technology.de>
2026-08-28 11:38:54 +0200

    checks: deactivate cyberus-linux.vms test in the pipeline for now, as it seems flaky

--- checks/default.nix
+++ checks/default.nix
@@ -8,20 +8,6 @@
       }
       // inputs.nixpkgs.lib.optionalAttrs (pkgs.stdenv.isLinux) {
         modules = pkgs.callPackage ./modules.nix { inherit (self) nixosModules; };
-      }
-      //
-        inputs.nixpkgs.lib.optionalAttrs
-          (
-            # The test for cyberus-linux.vms only works in 25.11 and 26.05 at the moment
-            inputs.nixpkgs.lib.versionAtLeast inputs.nixpkgs.lib.version "25.11"
-            && inputs.nixpkgs.lib.versionOlder inputs.nixpkgs.lib.version "26.11"
-            && pkgs.stdenv.isLinux
-            # Package ‘vm-test-run-vms’ [...] is not available on the requested hostPlatform: hostPlatform.system = "aarch64-linux"
-            && pkgs.stdenv.isx86_64
-          )
-          {
-            vms = pkgs.callPackage ./vms.nix { inherit (self) nixosModules; };
-          };
-
+      };
     };
 }
