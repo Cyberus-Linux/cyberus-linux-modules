c0291d65fda7735ba1c5938a393f8aad295e5575
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2026-09-24 10:45:10 +0200

    write-image: discard old data on disk

--- packages/write-image/src/src/main.rs
+++ packages/write-image/src/src/main.rs
@@ -2,13 +2,16 @@ use anyhow::{Context, Result};
 use clap::Parser;
 use crossterm::event::{self, Event, KeyCode};
 use ratatui::{
-    DefaultTerminal, Frame, layout::{Alignment, Constraint, Direction, HorizontalAlignment::Center, Layout, Rect}, style::{Color, Modifier, Style, Stylize as _}, widgets::{Block, Borders, Clear, Gauge, Paragraph},
+    DefaultTerminal, Frame,
+    layout::{Alignment, Constraint, Direction, HorizontalAlignment::Center, Layout, Rect},
+    style::{Color, Modifier, Style, Stylize as _},
+    widgets::{Block, Borders, Clear, Gauge, Paragraph},
 };
 use std::{
-    fs::File,
-    io::{Read as _, Write as _},
-    os::unix::fs::OpenOptionsExt,
+    io::{BufRead as _, BufReader, Read as _, Write as _},
+    os::unix::fs::{MetadataExt, OpenOptionsExt},
     path::{Path, PathBuf},
+    process::Stdio,
     sync::{Arc, Mutex},
     thread::sleep,
     time::Duration,
@@ -36,19 +39,24 @@ enum TaskStatus {
     #[default]
     NotStarted,
     InProgress(u16),
+    Skipped,
     Failed,
     Success,
 }
 
 impl TaskStatus {
     fn is_terminal(&self) -> bool {
-        matches!(self, TaskStatus::Failed | TaskStatus::Success)
+        matches!(
+            self,
+            TaskStatus::Failed | TaskStatus::Success | TaskStatus::Skipped
+        )
     }
 }
 
 #[derive(Debug, Clone, Copy, Default)]
 struct UiState {
     wiping: TaskStatus,
+    discarding: TaskStatus,
     writing: TaskStatus,
 }
 
@@ -121,7 +129,7 @@ impl App {
             .direction(Direction::Vertical)
             .constraints([
                 Constraint::Min(0),
-                Constraint::Length(6),
+                Constraint::Length(9),
                 Constraint::Min(0),
             ])
             .split(inner_area)[1];
@@ -137,7 +145,11 @@ impl App {
 
         let task_chunks = Layout::default()
             .direction(Direction::Vertical)
-            .constraints([Constraint::Length(3), Constraint::Length(3)])
+            .constraints([
+                Constraint::Length(3),
+                Constraint::Length(3),
+                Constraint::Length(3),
+            ])
             .split(horizontal_center);
 
         let render_task =
@@ -156,10 +168,13 @@ impl App {
                     ])
                     .split(chunks[0]);
 
-                frame.render_widget(Paragraph::new(label).style(Style::default().fg(THEME_COLOR_FG)), vertical_label[1]);
+                frame.render_widget(
+                    Paragraph::new(label).style(Style::default().fg(THEME_COLOR_FG)),
+                    vertical_label[1],
+                );
 
                 match status {
-                    TaskStatus::NotStarted | TaskStatus::Failed | TaskStatus::Success => {
+                    TaskStatus::NotStarted | TaskStatus::Failed | TaskStatus::Success | TaskStatus::Skipped => {
                         let vertical_text = Layout::default()
                             .direction(Direction::Vertical)
                             .constraints([
@@ -173,6 +188,7 @@ impl App {
                             TaskStatus::NotStarted => ("PENDING", THEME_COLOR_FG),
                             TaskStatus::Failed => ("FAILED", THEME_COLOR_FAILED),
                             TaskStatus::Success => ("OK", THEME_COLOR_FG),
+                            TaskStatus::Skipped => ("SKIPPED", Color::DarkGray),
                             _ => unreachable!(),
                         };
 
@@ -193,7 +209,13 @@ impl App {
             };
 
         render_task(frame, task_chunks[0], "Wiping partitions", &state.ui.wiping);
-        render_task(frame, task_chunks[1], "Writing data", &state.ui.writing);
+        render_task(
+            frame,
+            task_chunks[1],
+            "Discarding old data",
+            &state.ui.discarding,
+        );
+        render_task(frame, task_chunks[2], "Writing data", &state.ui.writing);
 
         fn render_popup(frame: &mut Frame, area: Rect, message: String, color: Color) {
             let vertical_chunks = Layout::default()
@@ -223,13 +245,13 @@ impl App {
 
             let inner_popup_area = popup_block.inner(popup_area);
             let popup_text_vertical_center = Layout::default()
-                      .direction(Direction::Vertical)
-                      .constraints([
-                          Constraint::Min(0),
-                          Constraint::Length(5),
-                          Constraint::Min(0),
-                      ])
-                      .split(inner_popup_area)[1];
+                .direction(Direction::Vertical)
+                .constraints([
+                    Constraint::Min(0),
+                    Constraint::Length(5),
+                    Constraint::Min(0),
+                ])
+                .split(inner_popup_area)[1];
 
             let popup_text = Paragraph::new(message)
                 .style(Style::default().add_modifier(Modifier::BOLD))
@@ -242,7 +264,8 @@ impl App {
 
         if state.is_terminal() {
             if let Some(error_msg) = &state.error {
-                let message = format!("An error occurred. Press ENTER to power down.\n\n{error_msg}");
+                let message =
+                    format!("An error occurred. Press ENTER to power down.\n\n{error_msg}");
                 render_popup(frame, inner_area, message, THEME_COLOR_FAILED);
             } else {
                 let message = String::from("Deployment complete.\n\nPress ENTER to power down.");
@@ -252,7 +275,7 @@ impl App {
     }
 }
 
-fn wipe_device_bare(device: &Path) -> Result<()> {
+fn wipe_device_inner(device: &Path) -> Result<()> {
     let cmd = std::process::Command::new("wipefs")
         .arg("-a")
         .arg(device)
@@ -274,7 +297,7 @@ fn wipe_device_bare(device: &Path) -> Result<()> {
 fn wipe_device(state: &App, device: &Path) -> Result<()> {
     state.update_state(|s| s.ui.wiping = TaskStatus::InProgress(0));
 
-    match wipe_device_bare(device) {
+    match wipe_device_inner(device) {
         Ok(_) => {
             state.update_state(|s| s.ui.wiping = TaskStatus::Success);
             Ok(())
@@ -286,20 +309,74 @@ fn wipe_device(state: &App, device: &Path) -> Result<()> {
     }
 }
 
-fn write_data(state: &App, input: &mut File, output: &mut File) -> Result<()> {
+fn discard_data_inner(state: &App, device: &Path) -> Result<()> {
+    state.update_state(|s| s.ui.discarding = TaskStatus::InProgress(0));
+    let device_size = std::fs::metadata(device)
+        .context("Failed to get device size")?
+        .size();
+    let chunk_mib = 256;
+    let chunk_size = chunk_mib << 20;
+
+    let mut cmd = std::process::Command::new("blkdiscard")
+        .arg("-p")
+        .arg(format!("{}M", chunk_mib))
+        .arg(device)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::null())  // FIXME: Make error output visible!
+        .spawn()
+        .context("Failed to execute wipefs")?;
+
+    let stdout = cmd
+        .stdout
+        .take()
+        .context("Failed to capture standard output")?;
+    let reader = BufReader::new(stdout);
+
+    for chunk_no in reader.lines().enumerate().map(|(i, _)| i as u64) {
+        state.update_state(|s| {
+            s.ui.discarding =
+                TaskStatus::InProgress(((chunk_no * chunk_size * 100) / device_size) as u16)
+        });
+    }
+
+    if cmd.wait().context("Failed to wait for blkdiscard")?.success() {
+        state.update_state(|s| s.ui.discarding = TaskStatus::Success);
+    } else {
+        state.update_state(|s| s.ui.discarding = TaskStatus::Skipped);
+    }
+
+    Ok(())
+}
+
+fn discard_data(state: &App, device: &Path) -> Result<()> {
+    discard_data_inner(state, device).inspect_err(|_e| {
+        state.update_state(|s| s.ui.discarding = TaskStatus::Failed);
+    })
+}
+
+fn write_data(state: &App, input_file: &Path, output_file: &Path) -> Result<()> {
+    let mut input_file = std::fs::File::open(input_file).context("Failed to open input file")?;
+    let mut output_file = std::fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .custom_flags(libc::O_DSYNC) // Be sure we write synchronously.
+        .open(output_file)
+        .context("Failed to open output file")?;
+
     let mut buf = vec![0; WRITE_CHUNK_SIZE];
 
-    let filesize: u64 = input
+    let filesize: u64 = input_file
         .metadata()
         .context("Failed to get input file size")?
         .len();
     let mut bytes_read: u64 = 0;
 
     while bytes_read < filesize {
-        let read = input
+        let read = input_file
             .read(&mut buf)
             .context("Failed to read from input file")?;
-        output
+        output_file
             .write_all(&buf[..read])
             .context("Failed to write to output file")?;
 
@@ -317,17 +394,8 @@ fn write_data(state: &App, input: &mut File, output: &mut File) -> Result<()> {
 
 fn write_image(state: &App, input_file: &Path, output_file: &Path) -> Result<()> {
     wipe_device(state, output_file)?;
-
-    let mut input_file = std::fs::File::open(input_file).context("Failed to open input file")?;
-    let mut output_file = std::fs::OpenOptions::new()
-        .write(true)
-        .truncate(true)
-        .create(true)
-        .custom_flags(libc::O_DSYNC) // Be sure we write synchronously.
-        .open(output_file)
-        .context("Failed to open output file")?;
-
-    write_data(state, &mut input_file, &mut output_file)?;
+    discard_data(state, output_file)?;
+    write_data(state, input_file, output_file)?;
 
     Ok(())
 }
